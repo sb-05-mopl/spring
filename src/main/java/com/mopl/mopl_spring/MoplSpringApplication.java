b520275aa8b84524bfec78b415d7be61f97b1eaf
userjin2123 <userjin2123@gmail.com>
2025-12-23 20:15:09 +0900

    chore : 정적 파일, build.gradle, .gitignore, application.yml 추가

---
+++ src/main/java/com/mopl/mopl_spring/MoplSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mopl.mopl_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoplSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoplSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mopl/mopl_spring/MoplSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mopl.mopl_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoplSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
