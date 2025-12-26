1ef6ae9fc776705ce5e8cadd31469babd583a675
userjin2123 <userjin2123@gmail.com>
2025-12-26 16:27:27 +0900

    chore: modify setup - Conversation의 필드 중 대화 참가자란 의미의 members가 있는데 Set으로 저장하게 되면 perPersist전 id가 null이기 때문에 추후 id가 결정되면 hash값이 변경되어 일관성이 깨질수 있기에 OneToMany 필드를 삭제하였습니다.

--- mopl-websocket-sse/src/main/java/com/mopl/moplwebsocketsse/domain/directMessage/entity/Conversation.java
+++ mopl-websocket-sse/src/main/java/com/mopl/moplwebsocketsse/domain/directMessage/entity/Conversation.java
@@ -1,16 +1,11 @@
 package com.mopl.moplwebsocketsse.domain.directMessage.entity;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import com.mopl.moplwebsocketsse.domain.common.entity.BaseEntity;
-import com.mopl.moplwebsocketsse.domain.user.entity.User;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -22,17 +17,9 @@ import lombok.NoArgsConstructor;
 @Table(name = "conversations")
 public class Conversation extends BaseEntity {
 
-	@OneToMany(mappedBy = "conversation", fetch = FetchType.LAZY)
-	private final Set<ConversationParticipants> members = new HashSet<>();
-
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "last_message_id")
 	private DirectMessage lastMessage;
-
-	public Conversation(User user1, User user2) {
-		this.members.add(new ConversationParticipants(this, user1));
-		this.members.add(new ConversationParticipants(this, user2));
-	}
 
 	public void updateLastMessage(DirectMessage message) {
 		this.lastMessage = message;
