7004e4978589193d62963722a4f79121b706e2c0
userjin2123 <userjin2123@gmail.com>
2025-12-31 14:43:39 +0900

    feat : Repository, Service, Controller 생성
    - Repository
    Follower와 Followee Id로 Follow 연관 관계가 있는 조회하는 메서드 추가
    
    - Service
    follow 생성 메서드 추가
    1. follower와 followee가 같은지 검증
    2. 이미 생성된 follow인지 검증
    3. follower와 followee가 실제로 있는지 검증
    
    follow 삭제 메서드 추가
    1. followId로 follow가 있는지 검증
    2. 요청한 유저와 follow의 follower Id 같은지 검증

---
+++ mopl-core/src/main/java/com/mopl/moplcore/domain/follow/repository/FollowRepository.java
@@ -0,0 +1,12 @@
+package com.mopl.moplcore.domain.follow.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mopl.moplcore.domain.user.entity.Follow;
+
+public interface FollowRepository extends JpaRepository<Follow, UUID> {
+
+	boolean existsByFollowerIdAndFolloweeId(UUID followerId, UUID followeeId);
+}
