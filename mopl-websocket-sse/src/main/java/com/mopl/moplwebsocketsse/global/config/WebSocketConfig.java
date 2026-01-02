dadec2a12283be8b5724ad988bff3897cf93ed04
userjin2123 <userjin2123@gmail.com>
2026-01-02 17:49:50 +0900

    feat : 하트비트 실행 시켜줄 태스크 추가
    - 하트비트 태스크를 자동 생성안해주고 따로 태스크를 지정해야 하더라고요! 생성했습니다.

--- mopl-websocket-sse/src/main/java/com/mopl/moplwebsocketsse/global/config/WebSocketConfig.java
+++ mopl-websocket-sse/src/main/java/com/mopl/moplwebsocketsse/global/config/WebSocketConfig.java
@@ -3,6 +3,8 @@ package com.mopl.moplwebsocketsse.global.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -17,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	private final JwtChannelInterceptor jwtChannelInterceptor;
+	private final ThreadPoolTaskScheduler wsHeartbeatScheduler;
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -29,7 +32,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.enableSimpleBroker("/sub")
-			.setHeartbeatValue(new long[]{10000L,10000L});
+			.setHeartbeatValue(new long[]{10000L,10000L})
+			.setTaskScheduler(wsHeartbeatScheduler);
 		registry.setApplicationDestinationPrefixes("/pub");
 	}
 
