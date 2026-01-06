88055f19678f99c16a46f1ed4d0b44b9059bb3bc
userjin2123 <userjin2123@gmail.com>
2026-01-07 00:40:35 +0900

    feat : RedisConfig 추가, WebSocketConfig에 하트비트 인터셉터 추가

--- mopl-websocket-sse/src/main/java/com/mopl/moplwebsocketsse/global/config/WebSocketConfig.java
+++ mopl-websocket-sse/src/main/java/com/mopl/moplwebsocketsse/global/config/WebSocketConfig.java
@@ -3,12 +3,12 @@ package com.mopl.moplwebsocketsse.global.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.mopl.moplwebsocketsse.domain.watch.interceptor.WatchingSessionHeartbeatInterceptor;
 import com.mopl.moplwebsocketsse.security.jwt.JwtChannelInterceptor;
 
 import lombok.RequiredArgsConstructor;
@@ -20,6 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	private final JwtChannelInterceptor jwtChannelInterceptor;
 	private final ThreadPoolTaskScheduler wsHeartbeatScheduler;
+	private final WatchingSessionHeartbeatInterceptor watchingSessionHeartbeatInterceptor;
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -39,6 +40,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureClientInboundChannel(ChannelRegistration registration) {
-	    registration.interceptors(jwtChannelInterceptor);
+	    registration.interceptors(
+			jwtChannelInterceptor,
+			watchingSessionHeartbeatInterceptor
+		);
 	}
 }
