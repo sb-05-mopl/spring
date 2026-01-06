f1b6b7a1ad5f0534798f76d20afe4fab7b637dae
userjin2123 <userjin2123@gmail.com>
2026-01-07 00:45:12 +0900

    feat : JwtTokenProvider token get 및 validate부분만 사용, JwtAuthenticationFilter 단순 토큰 검증, SecurityConfig 해당 부분 적용, SecurityConfigLocal Bean등록 중복이기에 일단 주석처리
    - Jwt 인증 부분은 현재 인메모리로 core에서 돌아가기 때문에 토큰 무효화같은 기능을 ws-sse 모듈 서버에 적용할 수 없습니다. ( 토큰 registry가 없어서 ), 추후 레디스에 Jwt 토큰이 올라오면 해당 부분들도 수정이 필요합니다.

--- mopl-websocket-sse/src/main/java/com/mopl/moplwebsocketsse/global/config/SecurityConfig.java
+++ mopl-websocket-sse/src/main/java/com/mopl/moplwebsocketsse/global/config/SecurityConfig.java
@@ -2,21 +2,27 @@ package com.mopl.moplwebsocketsse.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.mopl.moplwebsocketsse.security.mock.MockAuthenticationFilter;
+import com.mopl.moplwebsocketsse.security.jwt.JwtAuthenticationFilter;
 
-// @Configuration
-// @EnableWebSecurity
-// 로컬 테스트 인증 통과용 임시 코드
-public class SecurityConfigLocal {
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@Profile({"dev", "prod"})
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
 	@Bean
-	public SecurityFilterChain localFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf(csrf -> csrf.disable())
 			.formLogin(form -> form.disable())
@@ -25,11 +31,10 @@ public class SecurityConfigLocal {
 			.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/actuator/**").permitAll()
-				.requestMatchers("/api/**").permitAll()
-				.anyRequest().permitAll()
+				.requestMatchers("/ws/**").permitAll()
+				.anyRequest().authenticated()
 			)
-
-			.addFilterBefore(new MockAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
