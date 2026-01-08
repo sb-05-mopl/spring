14e97dc7cb59bf0ad376983db3c81494921a26c7
jeonggiju <halogiju123@gmail.com>
2026-01-08 18:38:27 +0900

    add: 커스텀 access, refresh 토큰 예외 추가 .

--- mopl-core/src/main/java/com/mopl/moplcore/security/exception/InValidAccessTokenException.java
+++ mopl-core/src/main/java/com/mopl/moplcore/security/exception/InValidAccessTokenException.java
@@ -3,8 +3,8 @@ package com.mopl.moplcore.security.exception;
 import com.mopl.moplcore.global.exception.BaseException;
 import com.mopl.moplcore.global.exception.ErrorCode;
 
-public class InValidAccessToken extends BaseException {
-	public InValidAccessToken() {
+public class InValidAccessTokenException extends BaseException {
+	public InValidAccessTokenException() {
 		super(ErrorCode.INVALID_ACCESS_TOKEN);
 	}
 }
