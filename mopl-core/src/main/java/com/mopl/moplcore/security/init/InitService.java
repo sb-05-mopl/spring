57ddb38180d8064c75958ea9255c14062698dd02
jeonggiju <halogiju123@gmail.com>
2026-01-08 22:15:58 +0900

    bug: InitService.java 잘못된 변수 수정

--- mopl-core/src/main/java/com/mopl/moplcore/security/init/InitService.java
+++ mopl-core/src/main/java/com/mopl/moplcore/security/init/InitService.java
@@ -50,7 +50,7 @@ public class InitService {
 		}
 
 		String encodedPassword = passwordEncoder.encode(adminPassword);
-		User user = new User("테스트 사용자", userPassword, encodedPassword);
+		User user = new User("테스트 사용자", userEmail, encodedPassword);
 
 		userRepository.save(user);
 	}
