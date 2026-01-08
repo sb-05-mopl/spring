c5ecdc5a4f22c7e11235efaa699a4708b2c8c0ac
jeonggiju <halogiju123@gmail.com>
2026-01-08 17:17:01 +0900

    error: invalidate exception 추가

---
+++ mopl-core/src/main/java/com/mopl/moplcore/security/exception/InvalidCredentialException.java
@@ -0,0 +1,12 @@
+package com.mopl.moplcore.security.exception;
+
+import com.mopl.moplcore.global.exception.BaseException;
+import com.mopl.moplcore.global.exception.ErrorCode;
+
+public class InvalidCredentialException extends BaseException {
+
+    public InvalidCredentialException(){
+        super(ErrorCode.INVALID_CREDENTIALS);
+    }
+
+}
