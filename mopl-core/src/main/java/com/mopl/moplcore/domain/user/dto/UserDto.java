29b2fb026225c73307b6882ede7c27e195c4a244
jeonggiju <halogiju123@gmail.com>
2026-01-08 18:59:10 +0900

    refactor: UserDto.java record -> class

--- mopl-core/src/main/java/com/mopl/moplcore/domain/user/dto/UserDto.java
+++ mopl-core/src/main/java/com/mopl/moplcore/domain/user/dto/UserDto.java
@@ -4,14 +4,17 @@ import com.mopl.moplcore.domain.user.entity.Role;
 import java.time.Instant;
 import java.util.UUID;
 
-public record UserDto(
-    UUID id,
-    Instant createdAt,
-    String email,
-    String name,
-    String profileImageUrl,
-    Role role,
-    boolean locked
-) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
+public class UserDto{
+	UUID id;
+	Instant createdAt;
+	String email;
+	String name;
+	String profileImageUrl;
+	Role role;
+	boolean locked;
 }
