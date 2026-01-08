123eb6d9864aa2f17f3adb30b12de07ec1666ee9
jeonggiju <halogiju123@gmail.com>
2026-01-09 03:24:19 +0900

    fix: LogoutSuccessHandler 불필요한 코드 제거

--- mopl-core/src/main/java/com/mopl/moplcore/security/handler/MoplLogoutSuccessHandler.java
+++ mopl-core/src/main/java/com/mopl/moplcore/security/handler/MoplLogoutSuccessHandler.java
@@ -1,12 +1,15 @@
 package com.mopl.moplcore.security.handler;
 
+import java.io.IOException;
+
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import com.mopl.moplcore.security.jwt.registry.JwtRegistry;
 import com.mopl.moplcore.security.jwt.JwtTokenProvider;
-import com.mopl.moplcore.security.principal.MoplUserDetails;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,20 +17,14 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class LogoutSuccessHandler
-	implements org.springframework.security.web.authentication.logout.LogoutSuccessHandler {
+public class MoplLogoutSuccessHandler implements LogoutSuccessHandler {
 
 	private final JwtTokenProvider tokenProvider;
-	private final JwtRegistry jwtRegistry;
 
 	@Override
 	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
-		Authentication authentication) {
+		Authentication authentication)  {
 		Cookie cookie = tokenProvider.generateRefreshTokenExpirationCookie();
-		MoplUserDetails userDetails = (MoplUserDetails)authentication.getPrincipal();
-
-		jwtRegistry.invalidateJwtInformationByUserId(userDetails.getUserDto().getId());
 		response.addCookie(cookie);
-
 	}
 }
