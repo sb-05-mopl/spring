dacb48cfd3bd4c5440ec6eeeb511e37b26ecf7ba
jeonggiju <halogiju123@gmail.com>
2026-01-09 03:15:59 +0900

    add: 개발 편의를 위한 초기 사용자 추가 설정

--- mopl-core/src/main/java/com/mopl/moplcore/security/init/InitService.java
+++ mopl-core/src/main/java/com/mopl/moplcore/security/init/InitService.java
@@ -2,7 +2,6 @@ package com.mopl.moplcore.security.init;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -49,7 +48,7 @@ public class InitService {
 			return;
 		}
 
-		String encodedPassword = passwordEncoder.encode(adminPassword);
+		String encodedPassword = passwordEncoder.encode(userPassword);
 		User user = new User("테스트 사용자", userEmail, encodedPassword);
 
 		userRepository.save(user);
