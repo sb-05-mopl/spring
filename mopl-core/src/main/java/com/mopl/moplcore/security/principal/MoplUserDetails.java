8d8ee077b1d31a42b6346ee8461f6cc67b03746e
jeonggiju <halogiju123@gmail.com>
2026-01-11 02:19:36 +0900

    add: MolUserDetails.java 만을 Authentication 객체에 저장하도록 OAuth2User, OidcUser 상속 - 상속함으로써 몇가지 메서드를 필수적으로 오버라이드해야하지만 필요는 없기에 따로 건들지는 않았스비다 .

--- mopl-core/src/main/java/com/mopl/moplcore/security/principal/MoplUserDetails.java
+++ mopl-core/src/main/java/com/mopl/moplcore/security/principal/MoplUserDetails.java
@@ -1,11 +1,18 @@
 package com.mopl.moplcore.security.principal;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.hibernate.type.descriptor.jdbc.OracleJsonBlobJdbcType;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import com.mopl.moplcore.domain.user.dto.UserDto;
 import com.mopl.moplcore.domain.user.entity.AuthProvider;
@@ -17,7 +24,7 @@ import lombok.Getter;
 @EqualsAndHashCode(of = "userDto")
 @Getter
 @AllArgsConstructor
-public class MoplUserDetails implements UserDetails {
+public class MoplUserDetails implements UserDetails, OAuth2User, OidcUser {
 
 	private UserDto userDto;
 	private String password;
@@ -51,8 +58,28 @@ public class MoplUserDetails implements UserDetails {
 		this.password = null;
 	}
 
-	public AuthProvider getAuthProvider(){
-		return this.userDto.getProvider();
+	@Override
+	public Map<String, Object> getAttributes() {
+		return Map.of();
 	}
 
+	@Override
+	public String getName() {
+		return String.valueOf(userDto.getName());
+	}
+
+	@Override
+	public Map<String, Object> getClaims() {
+		return Map.of();
+	}
+
+	@Override
+	public OidcUserInfo getUserInfo() {
+		return null;
+	}
+
+	@Override
+	public OidcIdToken getIdToken() {
+		return null;
+	}
 }
