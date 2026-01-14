13cda6087968472d459bca69b703ff3e54d77607
userjin2123 <userjin2123@gmail.com>
2026-01-14 18:07:10 +0900

    fix: ContentTagRepository.findByContentId 에서 N+1 문제 발견해서 수정

--- mopl-core/src/main/java/com/mopl/moplcore/domain/content/repository/ContentTagRepository.java
+++ mopl-core/src/main/java/com/mopl/moplcore/domain/content/repository/ContentTagRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mopl.moplcore.domain.content.entity.ContentTag;
 
 public interface ContentTagRepository extends JpaRepository<ContentTag, UUID> {
+	@Query("SELECT ct FROM ContentTag ct JOIN FETCH ct.tag WHERE ct.content.id = :contentId")
 	List<ContentTag> findByContentId(UUID contentId);
 
 	void deleteByContentId(UUID id);
