bb95f8fb35cfdd4a5256145b964af684eeb0cdf3
userjin2123 <userjin2123@gmail.com>
2026-01-14 17:26:07 +0900

    feat: 조회 메서드에 query 분석용 테스트 코드

--- mopl-core/src/main/java/com/mopl/moplcore/domain/content/config/ContentSyncRunner.java
+++ mopl-core/src/main/java/com/mopl/moplcore/domain/content/config/ContentSyncRunner.java
@@ -1,6 +1,7 @@
 package com.mopl.moplcore.domain.content.config;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import com.mopl.moplcore.domain.content.service.ContentSyncService;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "content.sync.enabled", havingValue = "true", matchIfMissing = true)
 public class ContentSyncRunner implements CommandLineRunner {
 
     private final ContentSyncService contentSyncService;
