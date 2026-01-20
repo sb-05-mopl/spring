63bd8b6db5f06e0d9f58e87c5c4d89e7667641ab
jeonggiju <halogiju123@gmailc.om>
2026-01-20 20:04:45 +0900

    deploy-test26

--- deploy/nginx/scripts/starts.sh
+++ deploy/nginx/scripts/starts.sh
@@ -6,6 +6,7 @@ docker rm nginx || true
 docker pull 989775483620.dkr.ecr.ap-northeast-2.amazonaws.com/mopl/nginx:latest
 
 cd /home/ubuntu/nginx
+
 docker compose -f docker-compose.prod.yml down --remove-orphans 2>/dev/null || true
 docker compose -f docker-compose.prod.yml up -d --build
 echo "--------------- nginx 끝 ------------------"
