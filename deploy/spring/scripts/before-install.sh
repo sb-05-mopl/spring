8d39c8da8b8bb79041f9a2f413031af0ff8b7bf2
jeonggiju <halogiju123@gmailc.om>
2026-01-26 00:04:07 +0900

    무중단 배포 테스트12: 하... deploy-spring.yml 에서 카프카 env 설정을 빼머겅ㅆ다..

--- deploy/spring/scripts/before-install.sh
+++ deploy/spring/scripts/before-install.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-if [ -f /home/ubuntu/spring/.env ]; then
-    cp /home/ubuntu/spring/.env /tmp/.env.backup
-fi
-
 rm -rf /home/ubuntu/spring/*
-rm -rf /home/ubuntu/spring/.*  2>/dev/null || true
-
-if [ -f /tmp/.env.backup ]; then
-    mkdir -p /home/ubuntu/spring
-    mv /tmp/.env.backup /home/ubuntu/spring/.env
-fi
+rm -rf /home/ubuntu/spring/.* 2>/dev/null || true
